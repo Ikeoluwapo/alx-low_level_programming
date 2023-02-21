3912dbfa9d896030396d38ab555bc0a563cb0664
Emmanuella <eikeoluwapo@gmail.com>
2023-02-20 20:58:39 -0800

    add

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main -Entry point
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+	printf("%d", i);
+	}
+	return (0);
+}

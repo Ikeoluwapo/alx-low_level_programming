ec92a73a5bc89e133dd3d747bf6193d98fe7e427
Emmanuella <eikeoluwapo@gmail.com>
2023-02-21 20:50:37 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+	{
+		putchar(alphabet);
+	}
+	putchar("\n");
+	return (0);
+}

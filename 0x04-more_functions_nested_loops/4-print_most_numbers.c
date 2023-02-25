8a2177e1dbfa831a15f4bcf96dc366b74293c063
Emmanuella <eikeoluwapo@gmail.com>
2023-02-25 03:21:51 -0800

    add

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print most numbers except 2 and 4
+ *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (i ==  2 && i ==  4)
+		{
+			continue;
+		}
+		else
+		{
+			putchar(i);
+		}
+			putchar('\n');
+	}
+}

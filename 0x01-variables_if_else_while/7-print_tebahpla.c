c4b5db3d4be00a361564102f7fb224ce7ffa935a
Emmanuella <eikeoluwapo@gmail.com>
2023-02-25 08:03:03 -0800

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * Print lowercase letters in reverse order
+ * Return 0
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 'z'; i >= 'a'; i--)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}

d6086c67f205fe3f0e05b47a728cbcd8d4150505
Emmanuella <eikeoluwapo@gmail.com>
2023-02-28 12:54:47 -0800

    puts

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,13 +9,12 @@
 
 void puts2(char *str)
 {
-	int i = 0;
+	int i;
 
-	while (str[i] != '\0')
+	for (i = 0; str[i] != '\0'; i++)
 	{
 		if (i % 2 == 0)
 			putchar(str[i]);
-		i += 2;
 	}
 	putchar('\n');
 }

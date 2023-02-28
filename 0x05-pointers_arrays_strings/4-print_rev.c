c4d3c7cd465beee6eb0c9c8139e7c428808298a7
Emmanuella <eikeoluwapo@gmail.com>
2023-02-27 17:46:43 -0800

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_rev - prints a string, in reverse
+ *
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int i >= 0;
+
+	while (str[i])
+	{
+		putchar(str[i]);
+		i--;
+	}
+	putchar('\n');
+}

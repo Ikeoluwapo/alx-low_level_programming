ca19263f9989f9051d023ddaa8bee1ec45f70d46
Emmanuella <eikeoluwapo@gmail.com>
2023-02-27 16:07:43 -0800

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,9 +3,9 @@
 #include <string.h>
 
 /**
- * strlen - returns the length of a string
- *
- * Return: Always 0
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
  */
 
 int _strlen(char *s)

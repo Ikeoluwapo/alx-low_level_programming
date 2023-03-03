ae2580c3db584af367d3568eacf2e50e89c41a58
Emmanuella <eikeoluwapo@gmail.com>
2023-03-03 07:56:03 -0800

    base16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,19 +1,23 @@
 #include <stdio.h>
 
-/*
- * main -  Entry point
- * Print Hexadecimal
- * Return: 0 if successful
+/**
+ * main - Prints numbers between 0 to 9 and letters between a to f.
+ *
+ * Return: Always 0 (Success)
  */
-
 int main(void)
 {
-	char i;
+	int i;
 
-	for (i = "%x"; i++;)
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	for (i = 97; i < 103; i++)
 	{
 		putchar(i);
 	}
 	putchar('\n');
-	Return(0);
+	return (0);
 }
+

929b68ecedeb075ff9c07363ef188e2af0dae22b
Emmanuella <eikeoluwapo@gmail.com>
2023-03-13 13:32:28 -0700

    receive

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -6,16 +6,14 @@
  * Return: 0
  */
 
-int main(int argc __attribute__((unused)), char *argv[])
+int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
-	printf("%s\n", argv[1]);
-	printf("%s\n", argv[2]);
-	printf("%s\n", argv[3]);
-	printf("%s\n", argv[4]);
-	printf("%s\n", argv[5]);
-	printf("%s\n", argv[6]);
-	printf("%s\n", argv[7]);
+	int i = 0;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
 	return (0);
 }
 

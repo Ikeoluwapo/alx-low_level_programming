17adae7ab0e7b1e5be99a98938b8e7832964f25b
Emmanuella <eikeoluwapo@gmail.com>
2023-03-22 00:39:18 -0700

    name

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check code
+ * @argv: argument vector
+ * @argc: argument count
+ * Return: 0
+ */
+
+int main(int argc__attribute__((unused)), char **argv)
+{
+	putchar(argv[0]);
+	return (0);
+}

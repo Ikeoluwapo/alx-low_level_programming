c8108b481be329f2f51acb520d582b34cb0a447b
Emmanuella <eikeoluwapo@gmail.com>
2023-03-24 09:39:52 -0700

    new

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -9,4 +9,11 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	struct dog newdog;
+	struct dog *new_d = &newdog;
 
+	newdog.name = name;
+	newdog.age = age;
+	newdog.owner = owner;
+	return (new_d);
+}

4a6c294e6da9315b012d55111ef274f849a5d55b
Emmanuella <eikeoluwapo@gmail.com>
2023-03-23 17:01:48 -0700

    dog

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * print_dog - print struct dog
+ * @d: pointer
+ * Return: 0
+ */
+
+void print_dog(struct dog *d)
+{
+	if (age == NULL || owner == NULL)
+	{
+		printf(nil);
+	}
+	if (name == NULL)
+	{
+		printf(Name : nil);
+	}
+	if else(d == NULL)
+	{
+		printf();
+	}
+	return (0);
+}

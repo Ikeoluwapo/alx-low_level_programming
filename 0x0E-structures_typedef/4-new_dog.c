44a5b1ffdacbe249110f5ae5524dafbddd2a102f
Emmanuella <eikeoluwapo@gmail.com>
2023-03-23 18:54:51 -0700

    main5

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+
+/**
+ * new_dog - function that creates a new dog
+ *
+ * Return: 0
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+

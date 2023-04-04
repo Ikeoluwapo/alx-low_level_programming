73ef364b492bb4b0dce8f988259294a3213f89f6
Ikeoluwapo <eikeoluwapo@gmail.com>
2023-04-04 06:33:35 -0700

    many

--- 0x12-singly_linked_lists/_putchar.c
+++ 0x12-singly_linked_lists/_putchar.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 
-/**                                               * _putchar - put
-* @c: character                                   * Return: 0
-*/
+/**
+ * _putchar - put
+ * @c: character
+ * Return: 0
+ */
 
 int _putchar(char c)
 {
-        return (0);
+	return (0);
 }

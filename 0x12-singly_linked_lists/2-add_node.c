263dd481db731a7e4d64ab5620f8554ce51856b2
Ikeoluwapo <eikeoluwapo@gmail.com>
2023-04-04 09:09:07 -0700

    noe

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -3,6 +3,14 @@
 #include <string.h>
 #include "lists.h"
 
+
+/**
+ * add_node - to add a new node
+ * @new_node: pojbtwr to new node
+ *
+ * Return: new node
+ */
+
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node;
@@ -22,17 +30,23 @@ list_t *add_node(list_t **head, const char *str)
 		}
 		new_node->len = strlen(str);
 		new_node->next = *head;
-	
+
 		*head = new_node;
 	}
 
 	return (new_node);
 }
 
+/**
+ * int_strlen: to check length of string
+ * @len: length
+ *
+ * Return: len
+ */
 unsigned int _strlen(const char *s)
 {
 	unsigned int len = 0;
-	
+
 	while (*s != '\0')
 	{
 		len++;

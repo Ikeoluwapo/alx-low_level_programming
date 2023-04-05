88fd8e7b40c5f8def184e8a4927761b5cdb2cb4b
Ikeoluwapo <eikeoluwapo@gmail.com>
2023-04-05 11:30:42 -0700

    leak

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "lists.h"
+
+/**
+ * free_list - to free the space
+ * @head: pointing to head
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *temp;
+
+	{
+		head = malloc(sizeof(list_t));
+		while(head != NULL)
+		{
+			temp = head;
+
+			head = head->next;
+			free(temp->str);
+			free(temp);
+		}
+	}
+}

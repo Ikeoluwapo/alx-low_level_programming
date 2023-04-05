29a81cd0fe1c57fde94af5f9cd718122215bdc01
Ikeoluwapo <eikeoluwapo@gmail.com>
2023-04-05 13:06:54 -0700

    kist

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * print_tortoise_string - to print a string
+ *
+ * Return: void
+ */
+
 void print_tortoise_string(void)
 {
 	printf("You're beat! and yet, you must allow,\n");

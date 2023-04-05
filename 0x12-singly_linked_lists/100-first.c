041fe8e4e36375f3182606f2d89a07362ec1c793
Ikeoluwapo <eikeoluwapo@gmail.com>
2023-04-05 13:03:45 -0700

    orint

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+void print_tortoise_string(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

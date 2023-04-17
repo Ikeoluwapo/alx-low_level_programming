67f953e5397f09c44b8bd6e72139f778fa8288cc
Ikeoluwapo <eikeoluwapo@gmail.com>
2023-04-17 05:37:37 -0700

    correct

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <errno.h>
 
 #define BUF_SIZE 1024
 
@@ -23,25 +24,25 @@ int main(int argc, char **argv)
 
 	if (argc != 3)
 	{
-		printf("Usage: cp file_from file_to\n");
+		dprintf(stderr, "Usage: cp file_from file_to\n", argv[0]);
 		exit(97);
 	}
 	if (access(argv[1], R_OK) != 0)
 	{
-		printf("Error: Can't read from file\n");
+		dprintf(stderr, "Error: Can't read from file\n", argv[1]);
 		exit(98);
 	}
 
 	fpfrom = fopen(argv[1], "r");
 	if (fpfrom == NULL)
 	{
-		printf("Error: Can't open from file %s\n", argv[1]);
+		dprintf(stderr,"Error: Can't open from file %s\n", argv[1], strerror(errno));
 		exit(1);
 	}
 	fpto = fopen(argv[2], "w");
 	if (fpto == NULL)
 	{
-		printf("Error: Can't open to write\n");
+		dprintf(stderr, "Error: Can't open to write\n", argv[2], strerror(errno));
 		fclose(fpfrom);
 		exit(1);
 	}
@@ -56,7 +57,7 @@ int main(int argc, char **argv)
 	fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (fd == -1)
 	{
-		printf("Error: Can't write to %s\n", argv[2]);
+		dprintf(stderr, "Error: Can't write to %s\n", argv[2]), strerror(errno));
 		fclose(fpto);
 		exit(99);
 	}
@@ -64,12 +65,12 @@ int main(int argc, char **argv)
 	{
 		if (byte_read == -1)
 		{
-			printf("Error: Can't read from file\n");
+			dprintf(stderr, "Error: Can't read from file\n", byte_read, strerror(errno));
 			exit(98);
 		}
 		if (write(fd, buf, byte_read) != byte_read)
 		{
-			printf("Error: Can't write to file\n");
+			dprintf(stderr, "Error: Can't write to file\n",byte_read, strerror(errno));
 			exit(99);
 		}
 	}
